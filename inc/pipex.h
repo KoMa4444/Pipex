cf202a68092a0fb25f26a573eb29aa29d1adfc45
KoMa4444 <marcelokollar@gmail.com>
2025-02-18 05:11:13 +0100

    subiendo el repo

---
+++ inc/pipex.h
@@ -0,0 +1,17 @@
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+//error handle
+int input_errors(int argc, char **argv);
+
+//main functions
+
+//utils
+
+#endif
---
+++ src/error_handle.c
@@ -0,0 +1,16 @@
+#include "../inc/pipex.h"
+
+int input_errors(int argc, char **argv)
+{
+    if (argc < 5)
+    {
+        printf("Error, argumentos no válidos\n\nPor favor siga el formato: *srcfile* *cmd1* *cmd2* *dstfile*\n");
+        return (0);
+    }
+    if (open(argv[1], O_RDONLY) == -1)
+    {
+        perror("Error al abrir el archivo");
+        return (0);
+    }
+    return (1);
+}
